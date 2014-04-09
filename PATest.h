fa690f8a856e11310f2b5d40e6f6fc3bb37d5674
Jack Liu <jackliu.mel@gmail.com>
2014-04-09 19:49:20 +0800

    added test class

---
+++ PATest.h
@@ -0,0 +1,13 @@
+//
+//  PATest.h
+//  Git Test
+//
+//  Created by Jack Liu on 9/04/2014.
+//  Copyright (c) 2014 Jack Liu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PATest : NSObject
+
+@end
---
+++ PATest.m
@@ -0,0 +1,13 @@
+//
+//  PATest.m
+//  Git Test
+//
+//  Created by Jack Liu on 9/04/2014.
+//  Copyright (c) 2014 Jack Liu. All rights reserved.
+//
+
+#import "PATest.h"
+
+@implementation PATest
+
+@end
